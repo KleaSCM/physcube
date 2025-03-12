4d5981db6b1693edd175d74455897fa8b7014803
Kleascm <kleascm@gmail.com>
2025-03-12 10:48:08 +1000

    Implement basic Three.js scene setup with a wireframe cube

--- app/page.tsx
+++ app/page.tsx
@@ -4,4 +4,23 @@ import {useEffect, useRef} from "react"
 import * as THREE from "three";
 export default function Physcube() {
   const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(()) => {
+    const scene = new THREE, Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(renderer.domElement);
+
+    // wireframe cube
+    const geometry = new THREE.BoxGeometry(5,5,5);
+    const material = new THREE.MeshBasicMaterial({color: 0x0077ff, wireframe: true});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube); 
+
+    camera.position.z = 10;
+
+
+
+
 }
